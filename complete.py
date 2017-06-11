11858ce50c245807382fa255d0bdcd2295ce6f66
Koki Yoshida <kreamkorokke@gmail.com>
2017-06-11 00:40:29 +0000

    Added custom complete function

--- complete.py
+++ complete.py
@@ -9,6 +9,7 @@ import os
 import tensorflow as tf
 
 from model import DCGAN
+from utils import imread, complete_images
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type=float, default=0.01)
@@ -19,12 +20,12 @@ parser.add_argument('--nIter', type=int, default=1000)
 parser.add_argument('--imgSize', type=int, default=64)
 parser.add_argument('--lam', type=float, default=0.1)
 parser.add_argument('--checkpointDir', type=str, default='checkpoint')
-parser.add_argument('--outDir', type=str, default='completions')
+parser.add_argument('--output-dir', type=str, dest='output_dir', default='completions')
 parser.add_argument('--outInterval', type=int, default=50)
-parser.add_argument('--maskType', type=str,
+parser.add_argument('--mask-type', type=str, dest='mask_type',
                     choices=['random', 'center', 'left', 'full', 'grid'],
                     default='center')
-parser.add_argument('--centerScale', type=float, default=0.25)
+parser.add_argument('--center-scale', type=float, dest='center_scale', default=0.25)
 parser.add_argument('imgs', type=str, nargs='+')
 
 args = parser.parse_args()
@@ -36,4 +37,35 @@ config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
     dcgan = DCGAN(sess, image_size=args.imgSize,
                   checkpoint_dir=args.checkpointDir, lam=args.lam)
-    dcgan.complete(args)
+    # Need to have a loaded model
+    assert(dcgan.load())
+    
+    # Construct mask
+    image_shape = imread(args.imgs[0]).shape
+    mask_type = args.mask_type
+    if mask_type == 'random':
+        fraction_masked = 0.2
+        mask = np.ones(image_shape)
+        mask[np.random.random(image_shape[:2]) < fraction_masked] = 0.0
+    elif mask_type == 'center':
+        center_scale = args.center_scale
+        assert(center_scale <= 0.5)
+        mask = np.ones(image_shape)
+        l = int(image_shape[0] * center_scale)
+        u = int(image_shape[0] * (1.0 - center_scale))
+        mask[l:u, l:u, :] = 0.0
+    elif mask_type == 'left':
+        mask = np.ones(image_shape)
+        c = image_size // 2
+        mask[:,:c,:] = 0.0
+    elif mask_type == 'full':
+        mask = np.ones(image_shape)
+    elif mask_type == 'grid':
+        mask = np.zeros(image_shape)
+        mask[::4,::4,:] = 1.0
+    else:
+        raise "Unknown mask type %s" % mask_type
+
+    adam_config = {'beta1': args.beta1, 'beta2': args.beta2, 'lr': args.lr, 'eps': args.eps}
+    complete_images(dcgan, num_iters=1000, input_image_paths=args.imgs, mask=mask,\
+                    output_dir=args.output_dir, )
--- utils.py
+++ utils.py
@@ -18,6 +18,73 @@ pp = pprint.PrettyPrinter()
 
 get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
 
+def complete_images(model, num_iters, input_image_paths, mask, output_dir,\
+                    adam_config, save_per_num_iters=100):
+    os.makedirs(os.path.join(output_dir, 'hats_imgs'), exist_ok=True)
+    os.makedirs(os.path.join(output_dir, 'completed'), exist_ok=True)
+
+    tf.global_variables_initializer().run()
+    image_shape = imread(input_image_paths[0])
+    # Assumes output images are square images
+    image_size, num_imgs = image_shape[0], len(input_image_paths)
+
+    batch_idxs = int(np.ceil(num_imgs/model.batch_size)) 
+    for idx in range(batch_idxs):
+        last_batch = idx == batch_idxs -1
+        lower_bound = idx * model.batch_size
+        upper_bound = num_imgs if last_batch else (idx+1) * model.batch_size
+	cur_size = upper_bound - lower_bound
+
+        cur_batch = input_image_paths[lower_bound : upper_bound]
+	cur_images = [get_image(cur_path, image_size, is_crop=model.is_crop) \
+                        for cur_path in cur_batch]
+	cur_images = np.array(cur_images).astype(np.float32)
+	if cur_size < model.batch_size:
+	    print("Padding the last batch with dummy images...")
+	    pad_size = ((0, int(model.batch_size - cur_size)), (0,0), (0,0), (0,0))
+	    cur_images = np.pad(cur_images, pad_size, 'constant').astype(np.float32)
+
+	batch_mask = np.resize(mask, [model.batch_size] + image_shape)
+        masked_images = np.multiply(cur_images, batch_mask)
+	input_z = np.random.uniform(-1, 1, size=(model.batch_size, model.z_dim))
+        # For Adam optimizer update on input noises
+	m, v = 0, 0
+
+	for i in range(num_iters):
+	    fd = {
+		model.z: input_z,
+		model.mask: batch_mask,
+		model.images: cur_images,
+		model.is_training: False
+	    }
+	    run = [model.complete_loss, model.grad_complete_loss, model.G]
+	    loss, g, G_imgs = model.sess.run(run, feed_dict=fd)
+            
+            beta1, beta2 = adam_config['beta1'], adam_config['beta2']
+            lr, eps = adam_config['lr'], adam_config['eps']
+
+	    m_prev, v_prev = np.copy(m), np.copy(v)
+	    m = beta1 * m_prev + (1 - beta1) * g[0]
+	    v = beta2 * v_prev + (1 - beta2) * np.multiply(g[0], g[0])
+	    m_hat = m / (1 - beta1 ** (i + 1))
+	    v_hat = v / (1 - beta2 ** (i + 1))
+	    input_z += - np.true_divide(lr * m_hat, (np.sqrt(v_hat) + eps))
+	    input_z = np.clip(input_z, -1, 1)
+
+	    if i % save_per_num_iters == 0:
+                print("After %d iterations, current average loss of batch %d is: %f" %\
+                        (i, idx, np.mean(loss[:cur_size]))
+		batch_dir = os.path.join(output_dir,'batch_idx_%d' % idx)
+                zhats_dir = os.path.join(batch_dir, 'zhats_iter_%d' % i)
+                completed_dir = os.path.join(batch_dir, 'completed_iter_%d' % i)
+                completed_images = masked_images + np.multiply(G_imgs, 1.0 - batch_mask)
+                for path_idx, path in enumerate(cur_batch):
+                    zhats_image_out_path = os.path.join(zhats_dir, str(path_idx)+'.png')
+                    completed_image_out_path = os.path.join(completed_dir, str(path_idx)+'.png')
+                    save_image(G_img[path_idx, :, :, :], zhats_image_out_path)
+                    save_image(completed_images[path_idx, :, :, :], completed_image_out_path)
+
+
 def get_image(image_path, image_size, is_crop=True):
     return transform(imread(image_path), image_size, is_crop)
 
